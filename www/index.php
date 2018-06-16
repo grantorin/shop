d5e21b52f028256a2fc01af72e7915bafba0a5f6
Gran Torino <red.chekist@gmail.com>
2018-06-16 16:51:34 +0300

    #1.2.1 - funkcional-zagruzki-stranic: add logic loadPage functions in index.php

--- www/index.php
+++ www/index.php
@@ -5,13 +5,33 @@ $controllerName = isset($_GET['controller'])
     ? ucfirst($_GET['controller']) // first letter to Uppercase, other to lowercase
     : 'Index';
 
-echo $controllerName;
+echo '<br>' . $controllerName;
 
 // определяем с каким екшеном работаем
 $actionName = isset($_GET['action'])
     ? $_GET['action'] // first letter to Uppercase, other to lowercase
     : 'index';
 
-echo $actionName;
+echo '<br>' . $actionName;
 
 include_once '../controllers/' . $controllerName . 'Controller.php';
+
+// формируем название вызываемой функции в контроллере
+$function = $actionName . 'Action';
+
+echo '<br>' . $function;
+
+$function(); // вызываем как функцию
+
+// Константы для обращения к контроллерам
+define('PathPrefix', '../controllers/');
+define('PathPostfix', 'Controller.php');
+
+function loadPage ($controllerName, $actionName = 'index') {
+    include_once PathPrefix . $controllerName . PathPostfix;
+
+    $function = $actionName . 'Action';
+    $function();
+}
+
+loadPage($controllerName, $actionName);
